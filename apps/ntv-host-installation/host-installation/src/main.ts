477732a0abd0017a1028808867cc3e55c07a4797
jerome <jeromed@n-compass.biz>
2025-07-28 09:58:08 +0800

    feat: ci cd deployment - trigger smart change detection

--- apps/ntv-host-installation/host-installation/src/main.ts
+++ apps/ntv-host-installation/host-installation/src/main.ts
@@ -3,5 +3,3 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
-
-// Test deployment trigger for host-installation only
